7bad3ea0419a20708fd1a665aeb3615e57b8527c
ByteMe6 <tsapiy.yurka@gmail.com>
2025-09-18 15:54:43 +0300

    v1.2 separate files
    i use function for image renderind in different file and then export it, so it can be used anywhere in the project now

--- src/templates/userCard.js
+++ src/templates/userCard.js
@@ -1,48 +1,10 @@
-const chromium = require('chromium');
-const fs = require('fs');
-const path = require('path');
-const handlebars = require('handlebars');
-const puppeteer = require('puppeteer-core');
-
-(async () => {
-
-  const templatePath = path.join(__dirname, 'userCard.hbs');
-  const templateSource = fs.readFileSync(templatePath, 'utf8');
-  const template = handlebars.compile(templateSource);
-
-  const cssPath = path.join(__dirname, 'userCard.css');
-  const css = fs.readFileSync(cssPath, 'utf8');
-
-  const data = { username: 'MessengerMAX', age: 2 };
-
-  const html = `
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <style>${css}</style>
-      </head>
-      <body>
-        ${template(data)}
-      </body>
-    </html>
-  `;
-
-  const browser = await puppeteer.launch({
-    executablePath: chromium.path,
-    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    headless: true,
-  });
-
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'networkidle0' });
+const chromium = require("chromium");
+const fs = require("fs");
+const path = require("path");
+const handlebars = require("handlebars");
+const puppeteer = require("puppeteer-core");
+const { renderImage } = require("../services/renderImage");
 
 
-  const element = await page.$('.user-card');
-  await element.screenshot({
-    path: path.join(__dirname, 'userCard.jpg'),
-    type: 'jpeg',
-  });
-
-  await browser.close();
-  console.log('сохранилась');
-})();
+const data = { username: "MessengerMAX", age: 2 };
+renderImage(data);
